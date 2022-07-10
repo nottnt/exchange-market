d5e4a05843c7faaef99727fdfbd84871824c96b7
nottnt <tanatat19@outlook.com>
2022-07-10 18:27:23 +0700

    feat: add service `ticker`

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,12 @@
-const BASE_URL = 'https://satangcorp.com/api/v3'
+const BASE_URL_SATANG = 'https://satangcorp.com/api/v3'
 
 export const get24HrsTicker = (symbol = null) => {
     const symbolParams = symbol ? `?symbol=${symbol}` : ''
 
-    return fetch(`${BASE_URL}/ticker/24hr${symbolParams}`, requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
+    return fetch(`${BASE_URL_SATANG}/ticker/24hr${symbolParams}`, {
+        method: 'GET',
+        redirect: 'follow'
+      })
+        .then(response => response.json())
         .catch(error => console.log('error', error));
 }
