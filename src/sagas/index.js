9cf1b204f90f69d9ce0416357a14cf244c9a15ab
nottnt <tanatat19@outlook.com>
2022-07-10 18:27:00 +0700

    feat: add market saga file

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,23 +1,7 @@
-import { all, takeEvery, put } from 'redux-saga/effects'
+import { all } from 'redux-saga/effects'
 import marketSaga from './market'
-
-function* exampleSaga() {
-    console.log("Example saga reached");
-}
-
-function* test() {
-    console.log('dddsasd')
-}
-
-function* watchTest() {
-    const { payload } = yield takeEvery('TEST', test)
-    console.log('a', payload)
-}
-
 export default function* Sagas() {
     yield all([
-        exampleSaga(),
-        marketSaga(),
-        watchTest()
+        marketSaga()
     ])
 }
--- src/sagas/market.js
+++ src/sagas/market.js
@@ -1,17 +1,40 @@
-import { all, call, take } from 'redux-saga/effects'
+import { all, call, take, delay, put, fork, cancel, select } from 'redux-saga/effects'
+import { get24HrsTicker } from '../services'
+import { updateCurrenciesData, updateCurrencyData } from '../actions/market'
+import { stopLoading } from '../actions/app'
 import constants from '../constants'
 
-function* fetchCurrencies(pairId) {
-
+function* fetch24HrsTicker(pairId) {
+    while (true) {
+        const state = yield select();
+        const data = yield call(get24HrsTicker, pairId)
+        if (pairId) yield put(updateCurrencyData(data))
+        else yield put(updateCurrenciesData(data))
+        if (state.app.isLoading) yield put(stopLoading())
+        yield delay(5000);
+    }
 }
 
-function* changeCurrencyPair() {
-    const action = yield take(constants.CHANGE_CURRENCY);
-    yield call(fetchCurrencies, action.payload.pairId)
+function* watchFetch24HrsTicker() {
+    while (true) {
+        const action = yield take(constants.FETCH_24H_TICKER);
+        const fetch24HrsTickerTask = yield fork(fetch24HrsTicker, action.payload.pairId)
+        yield take(constants.STOP_FETCH_24H_TICKER)
+        yield cancel(fetch24HrsTickerTask)
+    }
+}
+
+
+function* watchTestMarket() {
+    // while(true) {
+    const { payload } = yield take('TEST')
+    console.log('market test', payload)
+    // }
 }
 
 export default function* marketSaga() {
     yield all([
-        changeCurrencyPair
+        watchFetch24HrsTicker(),
+        watchTestMarket()
     ])
 }
